61c1a45e5ee8ebb1ea30bc372676e9def3b83e58
annieefu <af397@cornell.edu>
2022-12-25 09:07:23 -0500

    update github pages

---
+++ docs/_app/immutable/chunks/2-57b81e4c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-6e63c778.js";const e=!0;export{t as component,e as server};
--- docs/_app/immutable/chunks/2-bc3d9ff4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-4d4d2b31.js";const e=!0;export{t as component,e as server};
